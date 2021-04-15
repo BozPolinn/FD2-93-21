0b5bbf603f784ae97c59427adb93863d894a518a
ZinPolinn <bozkovapa@gmail.com>
2021-04-16 00:30:25 +0300

    empty

--- lesson1/dopDZoptimized.js
+++ lesson1/dopDZoptimized.js
@@ -1,29 +1,36 @@
 // объявляем переменную
 var user; 
 // запрашиваем строку у пользователя
-do {
-    user = prompt('Введите строку', 'Ваша строка');
-    boolEnter = Boolean(user);
-} while (boolEnter !== true);
+user = prompt('Введите строку', 'Ваша строка');
 
 function cutEmpty(param) {
-        // определение длины строки и сохранение значения в переменную
-    var length = param.length;
+    if (param === '') {
+        return ('Вы ввели пустую строку!');
+    }
+    else {
+            // определение длины строки и сохранение значения в переменную
+        var length = param.length;
             // определение пробелов
             // а) выявление пробелов в начале строки
-    var lastStart = -1;
-    for (var indexNumStart = 0; param[indexNumStart] === ' '; indexNumStart++) {
+        var lastStart = -1;
+        for (var indexNumStart = 0; param[indexNumStart] === ' '; indexNumStart++) {
         lastStart = indexNumStart;
-    }
+        }
             // определяем количество пробелов в начале строки
-    var numStart = lastStart + 1;
+        var numStart = lastStart + 1;
             // б) выявление пробелов в конце строки
             // обозначаем индекс последнего символа строки
-    var firstEnd;
-    for (var indexNumEnd = length - 1; param[indexNumEnd] === ' '; indexNumEnd--) {
+        var firstEnd;
+        for (var indexNumEnd = length - 1; param[indexNumEnd] === ' '; indexNumEnd--) {
         firstEnd = indexNumEnd;
+        }
+        if (param.length === numStart) {
+            return ('Вы ввели пустую строку!');
+        }
+        else {
+            return ('&' + param.substring(numStart, firstEnd) + '&');
+        }
     }
-    return ('&' + param.substring(numStart, firstEnd) + '&');
 }
 var result = cutEmpty(user);
 console.log(result);
