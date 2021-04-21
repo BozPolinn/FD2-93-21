9f91aa458d06106bdb3ea7f933be7b58ea3e9797
ZinPolinn <bozkovapa@gmail.com>
2021-04-21 23:48:39 +0300

    vowel2

--- lesson2/vowel2.js
+++ lesson2/vowel2.js
@@ -1,9 +1,18 @@
 let userString = prompt('Введите строку');
 
 function countVowel(param) {
-
+    let lowStr = param.toLowerCase();
+    let counter = 0;
+    const letterArray = ['а', 'e', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    
+    for (let i = 0; i < lowStr.length;  i++) {
+        if (letterArray.includes(lowStr[i])) {
+            counter++;
+        } 
+    }
     return counter;
 }
 
-// let answer = countVowel(userString);
-// console.log('Количество гласных букв в строке: ' + answer);
+let answer = countVowel(userString);
+
+console.log('Количество гласных букв в строке: ' + answer);
